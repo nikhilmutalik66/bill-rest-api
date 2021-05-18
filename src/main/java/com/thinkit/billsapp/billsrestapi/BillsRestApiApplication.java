c06f479b45849b32f6b35462a7fe839cf5bdbfad
nikhil <nikhilmutalik66@gmail.com>
2021-05-18 23:02:15 +0530

    initial

---
+++ src/main/java/com/thinkit/billsapp/billsrestapi/BillsRestApiApplication.java
@@ -0,0 +1,13 @@
+package com.thinkit.billsapp.billsrestapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BillsRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BillsRestApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/thinkit/billsapp/billsrestapi/BillsRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thinkit.billsapp.billsrestapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BillsRestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
